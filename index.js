f09bbb41cd947ecafdf8231f1e5a988a9fade35c
Louis Boucquet <louisboucquet@gmail.com>
2024-01-28 17:46:13 +0100

    store scores in variable instead of data attributes

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 function init() {
 	const persons = document.getElementsByClassName('person');
 	const personArray = Array.from(persons);
-	const max = Array.from(persons).length;
+	const numberOfContestants = Array.from(persons).length;
 	const iterations = 3000;
 	let count = 0;
+
+	const scores = Array.apply(null, Array(numberOfContestants)).map(() => 0);
 
 	const tick = () => {
 		if (count >= iterations) return;
 
-		const winner = Math.floor(Math.random() * max);
+		const winnerIndex = Math.floor(Math.random() * numberOfContestants);
 		const spanneke = document.createElement('span');
-		personArray[winner].append(spanneke);
-		personArray[winner].setAttribute(
-			'data-total',
-			personArray[winner].getElementsByTagName('span').length,
-		);
+		personArray[winnerIndex].append(spanneke);
+		scores[winnerIndex]++;
 		count++;
 		requestAnimationFrame(tick);
 	};
