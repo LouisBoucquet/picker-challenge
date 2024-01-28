c40fcf0bd71e7847c8d7f92dbe26632b6dfbe342
Louis Boucquet <louisboucquet@gmail.com>
2024-01-28 18:03:47 +0100

    shorten code

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ function init() {
 	const scores = personArray.map(() => 0);
 
 	const tick = () => {
-		if (Math.max(...scores) >= 100) return;
-
 		scores.forEach((_, i) => {
 			scores[i] += Math.random() * 0.1;
 			personArray[i].style.width = `${scores[i]}%`;
-		})
+		});
 
-		requestAnimationFrame(tick);
+		if (Math.max(...scores) < 100)
+			requestAnimationFrame(tick);
 	};
 
 	tick();
