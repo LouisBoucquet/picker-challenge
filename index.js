29fc37af64449403baf2db377b7144cd003719f3
Louis Boucquet <louisboucquet@gmail.com>
2024-01-28 17:32:14 +0100

    use request animation frame

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 function init() {
-	const persons = document.getElementsByClassName("person");
+	const persons = document.getElementsByClassName('person');
 	const personArray = Array.from(persons);
 	const max = Array.from(persons).length;
 	const iterations = 3000;
 	let count = 0;
 
-	const iterator = setInterval(() => {
-		if (count < iterations) {
-			let winner = Math.ceil(Math.random() * max);
-			winner = winner - 1;
-			const spanneke = document.createElement("span");
-			personArray[winner].append(spanneke);
-			personArray[winner].setAttribute(
-				"data-total",
-				personArray[winner].getElementsByTagName("span").length
-			);
-			count++;
-		} else {
-			clearInterval(iterator);
-		}
-	}, 10);
+	const tick = () => {
+		if (count >= iterations) return;
+
+		let winner = Math.ceil(Math.random() * max);
+		winner = winner - 1;
+		const spanneke = document.createElement('span');
+		personArray[winner].append(spanneke);
+		personArray[winner].setAttribute(
+			'data-total',
+			personArray[winner].getElementsByTagName('span').length,
+		);
+		count++;
+		requestAnimationFrame(tick);
+	};
+
+	tick();
 }
 
-document.getElementById("start").addEventListener("click", () => {
+document.getElementById('start').addEventListener('click', () => {
 	init();
 });
